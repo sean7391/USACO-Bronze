51a69c6975537aa63baa8e2fc4b063e120350156
Sean C <47064367+sean7391@users.noreply.github.com>
2020-09-20 20:57:14 -0400

    Create teleport.cpp

---
+++ 2017-18/February/teleport.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define pb push_back
+#define f first
+#define s second
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define trav(a, x) for (auto& a: x)
+#define ins insert
+#define pq priority_queue
+#define sz(x) (int)x.size()
+#define rsz resize
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+#define endl "\n"
+
+typedef long long ll;
+typedef pair<int, int> pii; 
+
+const int dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
+
+void setIO(string s){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    freopen((s + ".in").c_str(), "r", stdin);
+    freopen((s + ".out").c_str(), "w", stdout);
+}
+
+int a, b, x, y;
+
+int main() {
+    //setIO("teleport"); 
+    cin >> a >> b >> x >> y;
+    cout << min(abs(x - a) + abs(y - b), min(abs(y - a) + abs(x - b), abs(a - b)));
+    return 0;
+}
+
